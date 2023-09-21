3e7b6a70aee7234b575c25ef4075b1922f6100f9
Ahmed Ezz <ahmed_ezz2021995@yahoo.com>
2023-09-22 01:25:35 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Ahmed Ezz on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
